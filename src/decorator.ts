3ac4e2ed818a7a759eee542be987e0865dd08a6a
mcanever <R3V3Kfn2vyxBwpjE5T5w>
2020-02-10 20:35:56 +0100

    removed useless method and parameter

--- src/decorator.ts
+++ src/decorator.ts
@@ -3,14 +3,11 @@ import { GlobalScope } from './global';
 class AccorTrackingDecorator {
     private scope: GlobalScope;
 
-    constructor(nameSpace: string = '') {
+    constructor() {
         this.scope = new GlobalScope();
         this.scope.set('instance', this);
     }
 
-    public getCurrentUrl(): string {
-        return location.href;
-    }
 }
 
 new AccorTrackingDecorator();
