2d5a434b1a5312a2682af0151bdaaea7cecaff8b
mcanever <R3V3Kfn2vyxBwpjE5T5w>
2020-02-10 21:16:14 +0100

    Improved build performance 100x by adding typescript watch. Basic readme about how to build.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Accor Tracking Decorator utility
+
+## How to build
+
+### Straightforward build:
+
+`npm i`
+`npm run build`
+
+### Watch and build in production mode into `dist/`
+
+`npm i`
+`npm run watch`
+
+### Watch and build in development mode into `build-dev/`
+
+`npm i`
+`npm run build-dev`
--- webpack.config.js
+++ webpack.config.js
@@ -4,29 +4,58 @@ const webpack = require('webpack');
 
 const license = fs.readFileSync(path.resolve(__dirname, 'LICENSE')).toString();
 
-module.exports = {
-    entry: './src/decorator.ts',
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: /\.ts?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
+module.exports = function(env) {
+    env.mode = env.mode || 'production';
+    var webpackMode = 'production';
+    var watch = false;
+    var output = 'dist';
+
+    if (env.mode === 'development') {
+        watch = true;
+        webpackMode = 'development'
+        output = 'build-dev'
+    } else if (env.mode === 'watch') {
+        watch = true;
+        webpackMode = 'production'
+        output = 'dist'
+    }
+
+
+    return {
+        entry: './src/decorator.ts',
+        mode: webpackMode,
+        watch: watch,
+        devtool: 'source-map',
+        module: {
+            rules: [
+                {
+                    test: /\.ts?$/,
+                    include: path.resolve(__dirname, 'src'),
+                    use: [
+                        {
+                            loader: 'ts-loader',
+                            options: {
+                                transpileOnly: true,
+                                experimentalWatchApi: true,
+                            }
+                        }
+                    ],
+                },
+            ],
+        },
+        resolve: {
+            extensions: [ '.ts', '.js' ],
+        },
+        output: {
+            filename: 'decorator.js',
+            path: path.resolve(__dirname, output),
+            pathinfo: false,
+        },
+        plugins: [
+            new webpack.BannerPlugin({
+                banner: license
+            }),
         ],
-    },
-    resolve: {
-        extensions: [ '.ts', '.js' ],
-    },
-    output: {
-        filename: 'decorator.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-        new webpack.BannerPlugin({
-            banner: license
-        }),
-    ]
+    }
 };
 
