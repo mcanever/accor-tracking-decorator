fae9eab6d9f3a15fc5ee3e7ccf1ff79151e9492a
mcanever <R3V3Kfn2vyxBwpjE5T5w>
2020-02-11 19:46:04 +0100

    Comments/improvements to ga.ts

--- src/ga.ts
+++ src/ga.ts
@@ -1,5 +1,6 @@
 import { logger } from "./logger";
 
+// For historical reasons we store the client ID and linker param in two global variables
 declare global {
     interface Window {
         ga: any
@@ -8,7 +9,10 @@ declare global {
     }
 }
 
+// Ported in typescript-ish from accor-booking/booking.js
 export function detectGAParameters(cback: (params: {gacid: string|false,  _ga: string|false}) => void): void {
+    window.AccorBooking_GUA_ClientId = false;
+    window.AccorBooking_GUA_linkerParam = false;
     let cbackParams:{gacid: string|false,  _ga: string|false} = {gacid: false, _ga: false};
     //Wait for ga() to be available and get clientId
     let clientIdInterval = setInterval(function() {
@@ -17,11 +21,14 @@ export function detectGAParameters(cback: (params: {gacid: string|false,  _ga: s
             clientIdInterval = null;
             window.ga(function() {
                 let trackers = window.ga.getAll();
+                // Get the client ID and Linker param from the first tracker
                 if (typeof trackers[0] !== 'undefined') {
                     let clientId = trackers[0].get('clientId')
                     window.AccorBooking_GUA_ClientId = clientId;
                     cbackParams.gacid = clientId;
                     logger.log('Detected clientID (gacid): '+clientId);
+                    // linkerParam returned from the tracker will look like _ga=1231234.234234.5235
+                    // We only need the value.
                     let linkerParam = trackers[0].get('linkerParam');
                     if (linkerParam) {
                         var parts = linkerParam.split('=');
@@ -30,20 +37,21 @@ export function detectGAParameters(cback: (params: {gacid: string|false,  _ga: s
                             window.AccorBooking_GUA_linkerParam = linkerParam;
                             cbackParams._ga = clientId;
                             logger.log('Detected linker param (_ga): ' + linkerParam);
-                            document.body.dispatchEvent(new Event('accor.tracking_params_available'));
                         }
                     }
                 }
+                // Dispatch an event to notify that we have the tracking parameters available
+                document.body.dispatchEvent(new Event('accor.tracking_params_available'));
                 cback(cbackParams);
             });
         }
     }, 50);
-    //Give up after 10 seconds
+
+    //Cancel polling after 10 seconds (Google analytics may not be there or never load for some reason)
+    //In this case we need to call the callback and dispatch the event anyways in case someone relies on this
     setTimeout(function() {
         if (typeof clientIdInterval !== 'undefined' && clientIdInterval !== null) {
             clearInterval(clientIdInterval);
-            window.AccorBooking_GUA_ClientId = false;
-            window.AccorBooking_GUA_linkerParam = false;
             document.body.dispatchEvent(new Event('accor.tracking_params_available'));
             cback(cbackParams);
         }
