293dfde06a67b8fe9e3338b33fe4d1d4e6de8449
mcanever <R3V3Kfn2vyxBwpjE5T5w>
2020-12-21 22:14:40 +0100

    Improved ga parameters detection algo

--- src/ga.ts
+++ src/ga.ts
@@ -11,47 +11,84 @@ declare global {
 
 // Ported in typescript-ish from accor-booking/booking.js
 // Detects Google Analytics Client ID and Linker Param
-export function detectGAParameters(cback: (params: {_ga: string|false}) => void, source: any = window): void {
-    source.JoAndJoeBooking_GUA_ClientId = false;
+export function detectGAParameters(cback: (params: { _ga: string|false }) => void, source: any = window): void {
     source.JoAndJoeBooking_GUA_linkerParam = false;
-    let cbackParams:{ _ga: string|false} = {_ga: false};
-    //Wait for ga() to be available and get clientId
-    let clientIdInterval = setInterval(function() {
-        if (typeof source.ga !== 'undefined') {
-            source.ga(function() {
-                let trackers = source.ga.getAll();
-                // Get the client ID and Linker param from the first tracker
-                if (typeof trackers[0] !== 'undefined') {
-                    clearInterval(clientIdInterval);
-                    clientIdInterval = null;
-                    // linkerParam returned from the tracker will look like _ga=1231234.234234.5235
-                    // We only need the value.
-                    let linkerParam = trackers[0].get('linkerParam');
-                    if (linkerParam) {
-                        // Added to support gtag.js
-                        const tuples = linkerParam.split('&');
-                        linkerParam = tuples[0];
-                        const parts = linkerParam.split('=');
-                        if (parts.length == 2) {
-                            linkerParam = parts[1];
-                            source.JoAndJoeBooking_GUA_linkerParam = linkerParam;
-                            cbackParams._ga = linkerParam;
-                            logger.log('Detected linker param (_ga): ' + linkerParam);
-                        }
-                    }
-                }
-                cback(cbackParams);
-            });
-        }
-    }, 200);
 
-    //Cancel polling after 10 seconds (Google analytics may not be there or never load for some reason)
-    //In this case we need to call the callback and dispatch the event anyways in case someone relies on this
+    let cbackParams:{ _ga: string|false} = { _ga: false};
+    //Wait for ga() to be available and get clientId
+    let clientIdIntervalCounter = 0;
+    let lock = false;
+
+    const searchForGaDecoratorParams = () => {
+        // logger.log ('searchForGaDecoratorParams', clientIdIntervalCounter);
+        clientIdIntervalCounter++;
+        if (typeof source.ga !== 'undefined') {
+            // logger.log('ga() found!');
+            if (
+                (  typeof clientIdInterval !== 'undefined' &&
+                   typeof source.JoAndJoeBooking_GUA_linkerParam !== 'undefined' && source.JoAndJoeBooking_GUA_linkerParam != '' ) ||
+                clientIdIntervalCounter > 4000 // 10 minutes
+            ) {
+                if (clientIdIntervalCounter > 4000) {
+                    logger.log('Google Analytics decorator failed after 10 minutes');
+                }
+                clearInterval(clientIdInterval);
+                return;
+            }
+
+            // We need a lock because ga() callbacks runs async and in some cases complete after > 1000ms
+            if (!lock) {
+                lock = true;
+                // logger.log ('lock at', utils.getElapsedMS() );
+                setTimeout (function () {
+                    // logger.log('call ga(...)');
+                    source.ga(function() {
+                        // logger.log ('unlock at', utils.getElapsedMS() );
+                        lock = false;
+                        const trackerToUse = location.href.indexOf('useSecondTrackerToDecorate') !== -1 ? 1 : 0;
+                        const trackers = source.ga.getAll();
+                        if (typeof trackers[trackerToUse] !== 'undefined') {
+                            let linkerParam = trackers[trackerToUse].get('linkerParam');
+                            if (linkerParam) {
+                                const parts = linkerParam.split('=');
+                                if (parts.length > 1) {
+                                    linkerParam = parts[1];
+                                    source.JoAndJoeBooking_GUA_linkerParam = linkerParam;
+                                    cbackParams._ga = linkerParam;
+                                    logger.log ('Detected linker param (_ga): ' + linkerParam);
+                                    logger.log ('Detected GA parameters after (ms): ', utils.getElapsedMS());
+                                } else {
+                                    logger.log('WARN likerParam format Error', linkerParam)
+                                }
+                            } else {
+                                logger.log('WARN no linkerParam');
+                            }
+                            // Dispatch an event to notify that we have the tracking parameters available
+                            utils.dispatchEvent('accor_tracking_params_available');
+                            cback(cbackParams);
+                        } else {
+                            logger.log('WARN trackers['+trackerToUse+'] undefined');
+                        }
+                    });
+                }, 10);
+            }
+        } else {
+            // logger.log('ga() not found yet');
+        }
+    };
+
+    // run immediately then start retrying
+    // logger.log('START GA Params detection');
+    searchForGaDecoratorParams();
+    let clientIdInterval = setInterval(searchForGaDecoratorParams, 150);
+
+    //Give up after 10 minutes
     setTimeout(function() {
-        if (typeof clientIdInterval !== 'undefined' && clientIdInterval !== null) {
+        if (typeof clientIdInterval !== 'undefined') {
+            logger.log('giving up GA params detection');
             clearInterval(clientIdInterval);
             utils.dispatchEvent('accor_tracking_params_available');
             cback(cbackParams);
         }
-    }, 10000);
-}
+    }, 600000);
+};
